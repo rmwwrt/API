db65a33cc797f260f9e4beae91da72b9cb6dfd30
rmwwrt <kalasnikovam403@gmail.com>
2026-02-19 21:32:51 +0700

    create lesson group part and lesson time models

---
+++ Models/Building.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NATKSchedule.Models
+{
+    [Table("building")]
+    public class Building
+    {
+        [Key]
+        [Column("building_id")]
+        public int BuildingId { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+        [Column("address")]
+        [Required]
+        public string Address { get; set; } = null!;
+        public List<Classroom> Classrooms { get; set; } = new();
+    }
+}
---
+++ Models/Classroom.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NATKSchedule.Models
+{
+    [Table("classroom")]
+    public class Classroom
+    {
+        [Key]
+        [Column("classroom_id")]
+        public int ClassroomId { get; set; }
+        [Column("building_id")]
+        public int BuildingId { get; set; }
+        [ForeignKey("BuildingId")]
+        public Building Building { get; set; } = null!;
+        [Column("room_number")]
+        [Required]
+        public string RoomNumber { get; set; } = null!;
+    }
+}
