1922b4c28739a55a3b701c48affcb87c018d9622
rmwwrt <kalasnikovam403@gmail.com>
2026-02-19 23:13:15 +0700

    create ScheduleController.cs

---
+++ Controllers/ScheduleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NATKSchedule.Data;
+using NATKSchedule.Services;
+using NATKSchedule.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NATKSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+
+        // GET: api/schedule/group/{groupName}?start=2026-01-12&end=2026-01-17
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            // Вызываем бизнес-логику из сервиса
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+
+            // Возвращаем результат со статусом 200 OK
+            return Ok(result);
+        }
+
+
+    }
+}
