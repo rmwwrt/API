5028838bb2eb5b3c6c8327e3a3a6d1612b7185cb
rmwwrt <kalasnikovam403@gmail.com>
2026-02-19 22:29:20 +0700

    create IScheduleService.cs

---
+++ Services/IScheduleService.cs
@@ -0,0 +1,10 @@
+﻿using NATKSchedule.DTO;
+
+namespace NATKSchedule.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime
+       startDate, DateTime endDate);
+    }
+}
